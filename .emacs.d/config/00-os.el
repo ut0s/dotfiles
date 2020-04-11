9e2dfd4729b9f697db1aee7bbad923e2370745e5
Yuto TAGASHIRA <44089530+ut0s@users.noreply.github.com>
2020-04-12 00:06:26 +0900

    fix: set default font config
    install IPA font

--- .emacs.d/config/00-os.el
+++ .emacs.d/config/00-os.el
@@ -38,3 +38,20 @@
 ;; 	 )
 ;;        default-frame-alist)
 ;;       )
+
+;; font config
+;; 半角英字設定
+(set-face-attribute 'default nil :family "Consolas" :height 100)
+
+;; 全角かな設定
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (font-spec :family "IPAゴシック" :size 14))
+;; 半角ｶﾅ設定
+(set-fontset-font (frame-parameter nil 'font)
+                  'katakana-jisx0201
+                  (font-spec :family "ＭＳ ゴシック" :size 14))
+;; ずれ確認用
+;; 0123456789012345678901234567890123456789
+;; ｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵ
+
