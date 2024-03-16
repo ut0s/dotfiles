668f6c25e7327c7508e4136681adf6060adad0de
Yuto Tagashira <ut0s.dev@gmail.com>
2024-03-16 17:53:00 +0900

    add: Makefile and each command script

---
+++ etc/deploy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -u
+
+readonly DOT_FILES=(
+  Brewfile
+  .Xresources
+  .Xresources.d
+  .aspell.conf
+  .aspell.conf
+  .aspell.en.pws
+  .bash_alias
+  .bash_alias_mac
+  .bash_myconfig
+  .bashrc
+  .clang-format
+  .emacs.d
+  .gemini
+  .gitconfig
+  .gitignore_global
+  .nanorc
+  .profile
+  .pythonstartup
+  .tigrc
+  .urxvt
+  .codex
+  .serena
+  .gemini
+  .copilot
+  .claude
+)
+
+readonly DOT_CONFIG_FILES=(
+  alacritty
+  fish
+  ghostty
+  gitu
+  mpv
+  nnn
+  terminator
+  tmux
+)
+
+function deploy() {
+  for file in "${DOT_FILES[@]}"; do
+    if [ -a "$HOME"/"$file".dot ]; then
+      rm -v "$HOME"/"$file".dot
+      ln -vs "$HOME"/dotfiles/"$file" "$HOME"/"$file"
+    else
+      unlink "$HOME"/"$file"
+      ln -vs "$HOME"/dotfiles/"$file" "$HOME"/"$file"
+    fi
+  done
+
+  for file in "${DOT_CONFIG_FILES[@]}"; do
+    if [ -a "$HOME"/.config/"$file".dot ]; then
+      rm -v "$HOME"/.config/"$file".dot
+      ln -vs "$HOME"/dotfiles/.config/"$file" "$HOME"/.config/"$file"
+    else
+      unlink "$HOME"/.config/"$file"
+      ln -vs "$HOME"/dotfiles/.config/"$file" "$HOME"/.config/"$file"
+    fi
+  done
+}
+
+mkdir -p "$HOME"/.config
+deploy
+
+# for vscode
+# $HOME/Library/Application Support/Code/User/
